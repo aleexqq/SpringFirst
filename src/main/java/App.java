9a53ad4c03b834d05706461841a96c2b3235762f
aleexqq <shalygin.alexey1@gmail.com>
2024-07-20 16:58:17 +0700

    добавил cat  (scope prototype)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hwBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hwBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean =
+                applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(catBean == catBean2);
+        System.out.println(hwBean == hwBean2);
     }
 }
